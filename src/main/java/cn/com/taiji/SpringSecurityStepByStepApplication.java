2698465754e562bd76ecfd7cf2364025b9e5dca2
andy_zhaozhao@hotmail.com <andy_zhaozhao@hotmail.com>
2019-04-11 18:39:25 +0800

    Add: init project

--- src/main/java/cn/com/taiji/SpringSecurityStepByStepApplication.java
+++ src/main/java/cn/com/taiji/SpringSecurityStepByStepApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude=SecurityAutoConfiguration.class)
 public class SpringSecurityStepByStepApplication {
 
     public static void main(String[] args) {
